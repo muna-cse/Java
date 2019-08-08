a38d6ed0414ed87e67dc995625becd211e749ef5
nusrat jahan muna <49690168+muna-cse@users.noreply.github.com>
2019-08-08 08:35:16 -0700

    Add files via upload

---
+++ logical_operator.java
@@ -0,0 +1,22 @@
+
+package arithmetic;
+
+
+public class logical_operator {
+        public static void main(String[]args) {
+            int sub1=40;
+            int sub2=60;
+            //&&=AND    ||=OR
+            if((sub1>30)||(sub2<30)){
+                
+               System.out.println("yes the conditon is true");
+
+            }
+            else{
+                               System.out.println("no the conditon is false");
+
+            }
+        }
+
+    
+}
