6489032413ad49a58343fb7dcb109de6a03f50d4
nusrat jahan muna <49690168+muna-cse@users.noreply.github.com>
2019-08-08 08:35:46 -0700

    Add files via upload

---
+++ assignment_operator.java
@@ -0,0 +1,23 @@
+package arithmetic;
+
+
+public class assignment_operator {
+     public static void main(String[] args) {
+
+        int x=40;
+        int y=20;
+        //x++;
+        x+=5;//also can write x=x+5;
+        System.out.println("x="+x);
+        
+        x*=5;
+        System.out.println("x="+x);
+        
+        x-=y;
+        System.out.println("x="+x);
+        
+        x/=y;
+        System.out.println("x="+x);
+
+     }
+}
