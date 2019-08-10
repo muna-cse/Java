706df67cd94c0df075d7270a6de5c1d0278f1dc2
nusrat jahan muna <49690168+muna-cse@users.noreply.github.com>
2019-08-10 12:14:03 -0700

    Add files via upload

---
+++ unary_operator.java
@@ -0,0 +1,16 @@
+
+package arithmetic;
+
+public class unary_operator {
+    
+    public static void main (String[] args){
+         
+        int x = 5 , result ;
+        result = +x ;
+        System.out .println("result=" +result);  
+          
+        result = -x ;
+        System.out .println("result=" +result);     
+    }
+}
+
