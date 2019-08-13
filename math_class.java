bf632259f416c25f3e11597bcd82a5ad8fca751c
nusrat jahan muna <49690168+muna-cse@users.noreply.github.com>
2019-08-13 00:29:51 -0700

    Add files via upload

---
+++ math_class.java
@@ -0,0 +1,38 @@
+package arithmetic;
+
+
+public class math_class {
+    
+    public static void main (String[]args){
+        int x = 2;
+        int y = - 4 ;
+        //System. out .println( Math.max(x, y));
+        int max = Math.max(x,y);
+        System. out .println("maximum num = "+max);
+        
+        int min = Math.min(x,y);
+        System. out .println("minimum num = "+min);
+        
+        int absolute = Math.abs(y);
+        System. out .println("absolute num = "+absolute);
+        
+        double power = Math.pow(x,y);
+        System. out .println("x to the power y = "+power);
+        
+        int round = Math.round(8.6f);
+        System. out .println("round number = "+round);
+        
+        double pi = Math.PI;
+        System. out .println("pi = "+pi);
+
+        
+
+        
+
+        
+    }
+                    
+     
+             
+ 
+}
